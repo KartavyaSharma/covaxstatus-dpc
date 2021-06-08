5e566a9e9cb5138df56fe0d2a2af0400e091941e
KartavyaSharma <Kartavya@outlook.com>
2021-06-09 02:03:27 +0530

    fix: tailwind config should work now

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,20 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-    purge: ['.src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {
-            extend: {
-                fontFamily: {
-                    'heads': ['Chivo', 'Helvetica', 'Arial', 'sans-serif'],
-                },
-                colors: {
-                    violet: colors.violet,
-                },
-            },
-        },
+  purge: [],purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+    	fontFamily: {
+    		'heads': ['Chivo', 'Helvetica', 'Arial', 'sans-serif'],
+    	},
+    	colors: {
+    		violet: colors.violet,
+    	},
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 }
